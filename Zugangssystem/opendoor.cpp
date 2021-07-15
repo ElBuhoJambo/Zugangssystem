ae81476ec50cb735f2c2549312bdeb21fdd3caaa
user <user@Linux1801>
2021-07-15 14:26:35 +0200

    openDoor

--- Zugangssystem/opendoor.cpp
+++ Zugangssystem/opendoor.cpp
@@ -1,6 +1,20 @@
 #include "opendoor.h"
 
+openDoor* openDoor::oInstance = new openDoor();
+
+openDoor* openDoor::getInstance(){
+    return oInstance;
+}
+
 openDoor::openDoor()
 {
 
+}
+
+void openDoor::rightsReturned(bool access, QString loc){
+
+}
+
+void openDoor::checkAccess(QString RFID, QString loc){
+    emit Rights(RFID, loc);
 }
--- Zugangssystem/opendoor.h
+++ Zugangssystem/opendoor.h
@@ -1,11 +1,25 @@
 #ifndef OPENDOOR_H
 #define OPENDOOR_H
 
+#include <QObject>
 
-class openDoor
+class openDoor : public QObject
 {
+    Q_OBJECT
+
 public:
+    static openDoor* getInstance(void);
+
+signals:
+    void Rights(QString RFID, QString loc);
+
+public slots:
+    void rightsReturned(bool access, QString loc);
+    void checkAccess(QString RFID, QString loc);
+
+private:
     openDoor();
+    static openDoor* oInstance;
 };
 
 #endif // OPENDOOR_H
