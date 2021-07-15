f0522efe1ec603a55fd814cc65734e6fe15ec574
user <user@Linux1801>
2021-07-15 14:16:36 +0200

    accessRights

--- Zugangssystem/accessrights.cpp
+++ Zugangssystem/accessrights.cpp
@@ -1,6 +1,21 @@
 #include "accessrights.h"
 
+accessRights* accessRights::aInstance = new accessRights();
+
+accessRights* accessRights::getInstance(){
+    return aInstance;
+}
+
 accessRights::accessRights()
 {
 
+}
+
+void accessRights::checkAccess(QString RFID, QString loc){
+    emit SQLRequest(RFID, loc);
+}
+
+void accessRights::receiveResult(bool access, QString RFID, QString loc, QString name){
+    emit Door(access, loc);
+    emit Visual(name, RFID, access);
 }
--- Zugangssystem/accessrights.h
+++ Zugangssystem/accessrights.h
@@ -1,11 +1,28 @@
 #ifndef ACCESSRIGHTS_H
 #define ACCESSRIGHTS_H
 
+#include <QObject>
 
-class accessRights
+
+class accessRights : public QObject
 {
+    Q_OBJECT
+
 public:
+    static accessRights* getInstance(void);
+
+signals:
+    void SQLRequest(QString RFID, QString loc);
+    void Visual(QString RFID, QString loc, bool access);
+    void Door(bool access, QString loc);
+
+public slots:
+    void checkAccess(QString RFID, QString loc);
+    void receiveResult(bool access, QString RFID, QString loc, QString name);
+
+private:
     accessRights();
+    static accessRights* aInstance;
 };
 
 #endif // ACCESSRIGHTS_H
