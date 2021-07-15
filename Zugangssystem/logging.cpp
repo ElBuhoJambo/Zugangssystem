5cb7d9db8a656e476e29c60694a7fbabf72e412a
user <user@Linux1801>
2021-07-15 13:51:42 +0200

    Logging

--- Zugangssystem/logging.cpp
+++ Zugangssystem/logging.cpp
@@ -1,6 +1,232 @@
 #include "logging.h"
+#include "unistd.h"
 
-Logging::Logging()
+#include <QDateTime>
+#include <QDebug>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+Logging* Logging::_instance = new Logging();
+
+/* Returns a pointer to the instance
+ * \return Logging*   instance pointer
+ */
+Logging* Logging::getInstance(void)
 {
+    return _instance;
+}
 
+/* Constructor of the class.
+ * The logging class starts its own thread including its own message handler.
+ * \return -
+ */
+Logging::Logging(QObject *parent)
+    : QThread(parent)
+{
+    lLogQueue.clear();
+
+    for (int i = 0; i < LOG_NO_LOGFILES; i++)
+    {
+        lPLogFile[i] = new QFile(QString("%1.%2").arg(lLogNames[i]).arg("log"));
+    }
+
+    lMaxLogFileSize = LOGGING_LOG_FILE_SIZE;
+
+    unlink(NAME_PIPE_IN);
+    mkfifo(NAME_PIPE_IN, 0666);
+    lFdPipeIn = open(NAME_PIPE_IN, O_RDONLY | O_NONBLOCK);
+    moveToThread(this);
+
+
+    lIsTimeValid = true;
+    logMessage("App;Logging available - waiting for timestamp",(int)LOG_COMMON);
+    lIsTimeValid = false;
+
+    start();
+}
+
+/* Destructor of the class.
+ * \return -
+ */
+Logging::~Logging()
+{
+    for (int i = 0; i < LOG_NO_LOGFILES; i++)
+    {
+        lPLogFile[i]->close();
+    }
+    close(lFdPipeIn);
+}
+
+/* Cyclic part of the logging thread
+ * \return -
+ */
+void Logging::run()
+{
+    lPCheckExtLog = new QTimer();
+    lPCheckExtLog->setInterval(1000);
+    lPCheckExtLog->setSingleShot(false);
+    connect(lPCheckExtLog,SIGNAL(timeout()),this,SLOT(checkExternalLogging()));
+    lPCheckExtLog->start();
+
+
+    QThread::exec();
+}
+/*
+ *
+ */
+void Logging::checkExternalLogging(void)
+{
+    QByteArray data;
+    data.clear();
+    data.reserve(1024);
+    size_t bytesRead;
+    if ( 0 != (bytesRead = read(lFdPipeIn,data.data(),1024)))
+    {
+        data.resize(bytesRead);
+        logMessage(QString("External log;%1").arg(QString::fromUtf8(data.data()).simplified()),(int)LOG_COMMON);
+    }
+
+}
+
+/* Sets the maximum logfile size in bytes (default is 1MB)
+ * \param   maxFileSize requested logfile size
+ * \return -
+ */
+void Logging::setLogFileSize(quint32 const maxFileSize)
+{
+    lMaxLogFileSize = maxFileSize;
+}
+
+/* Local time now is valid -> empty the queue.
+ * \param -
+ * \return -
+ */
+void Logging::logTimeIsvalid(void)
+{
+    lIsTimeValid = true;
+    for (int i = 0; i < lLogQueue.count(); i++)
+    {
+        LOG_QueueEl_t* pEntry = lLogQueue.at(i);
+
+        logMessage(pEntry->msg,pEntry->index);
+        delete pEntry;
+    }
+    lLogQueue.clear();
+}
+
+/* Slot for the log message.
+ * Date and time will be added.
+ * The log message is written to the logfile immediately.
+ * The file system is synced afterwards.
+ * \param   msg     QString containing the log message
+ * \param   index   file index
+ * \return -
+ */
+void Logging::logMessage(QString msg,int index)
+{
+    if (lIsTimeValid)
+    {
+        QFile* pLogFile = lPLogFile[index];
+
+        while (pLogFile->isOpen())
+        {
+            msleep(5);
+        }
+
+        /* keep the file closed */
+        if (pLogFile->open(QIODevice::Append))
+        {
+            QDateTime time = QDateTime::currentDateTime();
+
+            QString message = time.toString("yyyyMMdd;hhmmss;") + msg + "\n";
+            pLogFile->write(message.toLatin1(),message.length());
+            pLogFile->close();
+            /* sync with the disc to avoid losing data on power down */
+            fsync(pLogFile->handle());
+        }
+        /* create a backup log file if the maximum size is reached */
+        if ( lMaxLogFileSize <= pLogFile->size())
+        {
+            QFile* pBackupLogFile = new QFile(QString("%1.%2").arg(lLogNames[index]).arg("bak"));
+            if ( pBackupLogFile->exists())
+            {
+                pBackupLogFile->remove();
+            }
+            if (pLogFile->rename(QString("%1.%2").arg(lLogNames[index]).arg("bak")))
+            {
+                fsync(pLogFile->handle());
+                pLogFile->setFileName(QString("%1.%2").arg(lLogNames[index]).arg("log"));
+            }
+            delete pBackupLogFile;
+        }
+    }
+    else
+    {
+        LOG_QueueEl_t* pEntry = new LOG_QueueEl_t;
+        pEntry->msg = msg;
+        pEntry->index = index;
+        lLogQueue.append(pEntry);
+    }
+}
+
+
+QString Logging::getLogFileName(int index)
+{
+    if (index < (int)LOG_NO_LOGFILES)
+    {
+        return QString(lLogNames[index]);
+    }
+    return QString("");
+}
+
+
+void Logging::copyLogfiles(QString destination)
+{
+    logMessage(QString("copy logfiles to %1").arg(destination),(int)LOG_COMMON);
+
+    for (int i = 0; i < LOG_NO_LOGFILES; i++)
+    {
+        /* remove at destination */
+        QFile* pTmpFile = new QFile(QString("%1/%2.bak").arg(destination).arg(lLogNames[i]));
+        if (pTmpFile->exists())
+        {
+            pTmpFile->remove();
+        }
+        delete pTmpFile;
+        pTmpFile = new QFile(QString("%1/%2.log").arg(destination).arg(lLogNames[i]));
+        if (pTmpFile->exists())
+        {
+            pTmpFile->remove();
+        }
+        delete pTmpFile;
+        /* copy the files */
+        pTmpFile = new QFile(QString("%1.log").arg(lLogNames[i]));
+        if ( pTmpFile->exists())
+        {
+            if ( pTmpFile->copy(QString("%1/%2").arg(destination).arg(pTmpFile->fileName())))
+            {
+                logMessage(QString("copy %1 successful").arg(pTmpFile->fileName()),(int)LOG_COMMON);
+            }
+            else
+            {
+                logMessage(QString("copy %1 failed").arg(pTmpFile->fileName()),(int)LOG_COMMON);
+            }
+        }
+        delete pTmpFile;
+        pTmpFile = new QFile(QString("%1.bak").arg(lLogNames[i]));
+        if ( pTmpFile->exists())
+        {
+            if ( pTmpFile->copy(QString("%1/%2").arg(destination).arg(pTmpFile->fileName())))
+            {
+                logMessage(QString("copy %1 successful").arg(pTmpFile->fileName()),(int)LOG_COMMON);
+            }
+            else
+            {
+                logMessage(QString("copy %1 failed").arg(pTmpFile->fileName()),(int)LOG_COMMON);
+            }
+        }
+        delete pTmpFile;
+    }
+    emit stopCopyLogfiles();
 }
--- Zugangssystem/logging.h
+++ Zugangssystem/logging.h
@@ -1,11 +1,65 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
+#include <QString>
+#include <QQueue>
+#include <QThread>
+#include <QFile>
+#include <QTimer>
+#include <QVector>
 
-class Logging
+#define LOGGING_LOG_FILE_SIZE   2000000
+#define NAME_PIPE_IN "/tmp/Zugangssystem_Logging"
+
+typedef enum
 {
+    LOG_COMMON,
+    LOG_BUTTON,
+    LOG_SCAN,
+    LOG_NO_LOGFILES
+}LOG_Type_e;
+typedef struct
+{
+    QString msg;
+    int index;
+}LOG_QueueEl_t;
+
+class Logging : public QThread
+{
+    Q_OBJECT
+
 public:
-    Logging();
+    static Logging* getInstance(void);
+    void setLogFileSize(quint32 const maxFileSize);
+    QString getLogFileName(int index);
+
+signals:
+    void stopCopyLogfiles(void);
+
+public slots:
+    void logMessage(QString msg, int index = 0);
+    void copyLogfiles(QString destination);
+    void logTimeIsvalid(void);
+
+private slots:
+    void checkExternalLogging(void);
+
+protected:
+    void    run() override;
+private:
+    Logging(QObject *parent = 0);
+    ~Logging();
+    QFile* lPLogFile[LOG_NO_LOGFILES];
+    static Logging* _instance;
+    quint32 lMaxLogFileSize;
+    bool lIsTimeValid;
+
+    const char* lLogNames[LOG_NO_LOGFILES] = { "Allgemein", "Verbindung", "Tasten" };
+
+    int lFdPipeIn;
+    QTimer* lPCheckExtLog;
+
+    QVector<LOG_QueueEl_t*> lLogQueue;
 };
 
 #endif // LOGGING_H
