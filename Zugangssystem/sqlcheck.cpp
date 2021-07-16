10ca56656239f087ae0dbf3879f47b88791e67dd
Daniel <djambura1603@gmail.com>
2021-07-16 09:16:13 +0200

    SQL

--- Zugangssystem/sqlcheck.cpp
+++ Zugangssystem/sqlcheck.cpp
@@ -1,6 +1,99 @@
 #include "sqlcheck.h"
 
-sqlCheck::sqlCheck()
-{
+sqlCheck* sqlCheck::sInstance = new sqlCheck();
 
+sqlCheck* sqlCheck::getInstance(){
+    return sInstance;
+}
+
+sqlCheck::sqlCheck(){
+    const QString DRIVER("QSQLITE");
+
+    if(QSqlDatabase::isDriverAvailable(DRIVER)){
+        sDB = QSqlDatabase::addDatabase(DRIVER);
+        qDebug() << "Driver set successfully";
+        sDB.setDatabaseName("sqliteTest.db");
+        if(!sDB.open()){
+            qWarning() << "ERROR: " << sDB.lastError();
+        }
+
+        QSqlQuery query;
+
+        /*if(!query.exec("CREATE TABLE worker (id INTEGER PRIMARY KEY, name TEXT, RFID TEXT, loc TEXT, access INTEGER)")){
+            qWarning() << "ERROR: " << query.lastError().text();
+        }*/
+
+        /*if(!query.exec("INSERT INTO worker(name, RFID, loc, access) VALUES('Magda', '0004787793', 'Location 1', '1')")){
+            qWarning() << "ERROR: " << query.lastError().text();
+        }*/
+    }else{
+        qDebug() << "Driver Error";
+    }
+}
+
+void sqlCheck::showTable(){
+    QSqlQuery query;
+    query.prepare("SELECT name, RFID, loc, access FROM worker");
+    if(!query.exec()){
+        qWarning() << "ERROR: " << query.lastError().text();
+    }
+    while(query.next()){
+        emit ShowTable(QString(query.value(0).toString()), QString(query.value(1).toString()), QString(query.value(2).toString()), query.value(3).toString());
+    }
+}
+
+void sqlCheck::receiveRequest(QString RFID, QString loc){
+    QStringList results;
+    if(RFID.length() == 10){
+        qDebug() << "RFID is in correct format at sqlCheck";
+        results = request(RFID, loc);
+        if(results.at(1).toInt()){
+            emit Result(true, RFID, loc, results.at(0));
+        }else{
+            emit Result(true, RFID, loc, results.at(0));
+        }
+    }else{
+        qDebug() << "RFID is in faulty format at sqlCheck";
+    }
+}
+
+QStringList sqlCheck::request(QString RFID, QString loc){
+    QStringList results;
+
+    if(!sDB.isOpen()){
+        qDebug() << "DB is closed";
+        return QStringList("DB closed");
+    }
+
+    QSqlQuery query;
+    query.prepare("SELECT name, access FROM worker WHERE RFID = ?");
+    query.addBindValue(RFID);
+
+    if(query.exec()){
+        if(query.next()){
+            qDebug() << "DB found" << RFID << query.value(0).toString();
+            results << query.value(0).toString() << query.value(1).toString();
+            return results;
+        }else{
+            query.clear();
+            query.prepare("SELECT name, access FROM worker WHERE RFID = 'ClassID.Fehler'");
+            if(query.exec()){
+                if(query.next()){
+                    qDebug() << "DB not found" << RFID << query.value(0).toString();
+                    return QStringList(query.value(0).toString());
+                }else{
+                    qDebug() << "Error query next";
+                    return QStringList("Worker not found");
+                }
+            }
+
+        }
+    }else{
+        qDebug() << "Error query execution";
+        return QStringList("Worker not found");
+    }
+}
+
+bool sqlCheck::isOpen(){
+    return sDB.isOpen();
 }
--- Zugangssystem/sqlcheck.h
+++ Zugangssystem/sqlcheck.h
@@ -1,11 +1,34 @@
 #ifndef SQLCHECK_H
 #define SQLCHECK_H
 
+#include <QObject>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
 
-class sqlCheck
+class sqlCheck : public QObject
 {
+    Q_OBJECT
+
 public:
+    static sqlCheck* getInstance(void);
+    bool isOpen();
+    QStringList request(QString RFID, QString loc);
+
+signals:
+    void Result(bool access, QString RFID, QString loc, QString name);
+    void ShowTable(QString name, QString loc, QString RFID, QString access);
+    void logMessage(QString msg, int index);
+
+public slots:
+    void receiveRequest(QString RFID, QString loc);
+    void showTable();
+
+private:
     sqlCheck();
+    static sqlCheck* sInstance;
+    QSqlDatabase sDB;
 };
 
 #endif // SQLCHECK_H
